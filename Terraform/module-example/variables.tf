3c5b59db5d743c4df15d7b9b10044e6a771032bc
Bhanumolakahtalla <139118951+Bhanumolakahtalla@users.noreply.github.com>
2026-01-23 11:33:53 +0530

    Create variables.tf

---
+++ Terraform/module-example/variables.tf
@@ -0,0 +1,17 @@
+variable "dbt_cloud_token" {}
+variable "dbt_cloud_account_id" {}
+
+variable "dbt_host_url" {
+	description = "(optional) dbt Cloud API host URL"
+	type        = string
+	default     = "https://pi849.us1.dbt.com/api"
+}
+
+variable "gcp_project_id" {
+	description = "GCP project id used by the Google provider and passed into the module"
+	type        = string
+}
+
+variable "project_name" {
+	type = string
+}
