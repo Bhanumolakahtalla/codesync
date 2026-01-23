a840ab9c8de6964efff037d255ed7b0a002035ea
Bhanumolakahtalla <139118951+Bhanumolakahtalla@users.noreply.github.com>
2026-01-23 11:48:59 +0530

    Refactor outputs in outputs.tf for clarity

--- Terraform/TF_Module/outputs.tf
+++ Terraform/TF_Module/outputs.tf
@@ -5,8 +5,3 @@ output "project_id" {
 output "connection_id" {
   value = dbtcloud_global_connection.bigquery.id
 }
-
-output "environments" {
-  description = "Map of environment logical ids to environment ids in dbt Cloud"
-  value = { for k, v in dbtcloud_environment.env : k => v.environment_id }
-}
