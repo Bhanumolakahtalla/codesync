648597023a0e44a216c48f8d5d4d3bd82eb64b92
Bhanumolakahtalla <139118951+Bhanumolakahtalla@users.noreply.github.com>
2026-01-23 13:48:16 +0530

    Update variables.tf

--- Terraform/TF_Module/variables.tf
+++ Terraform/TF_Module/variables.tf
@@ -6,4 +6,11 @@ variable "gcp_service_account_key" {
   description = "GCP service account key JSON (from GitHub Secret)"
   type        = string
   sensitive   = true
+
+}
+
+variable "dbt_cloud_token" {
+  type        = string
+  description = "DBT_CLOUD_TOKEN"
+  sensitive   = true
 }
